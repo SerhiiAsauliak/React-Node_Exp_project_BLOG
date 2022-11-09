1003dec0a10607bb478c294f79764ab924eaa89b
Serhii <mel.gipppson@gmail.com>
2022-11-10 01:08:38 +0200

    installed express-validator, added first validation schema

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ import express from 'express'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+import { regValidation } from './validations/auth.js'
 
-dotenv.config() 
+dotenv.config()
 const passDB = process.env.passDB
 mongoose.connect(`mongodb+srv://Sergo:${passDB}@cluster0.ty0umqn.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('DB connection completed'))
-.catch(err => console.log(`DB connection failed: ${err}`))
+    .catch(err => console.log(`DB connection failed: ${err}`))
 
 const PORT = process.env.PORT || 5050
 const app = express()
 
-app.use(express.json())  
-app.get('/', (req, res) => {
-    res.send('hello')
+app.use(express.json())
+
+app.post('/auth/register', regValidation, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array())
+    }
+    res.json({ success: true })
 })
 
 app.listen(PORT, (err) => {
-    if(err){
+    if (err) {
         return console.log(err)
     }
-    console.log(`Server started on PORT ${PORT}`)})
+    console.log(`Server started on PORT ${PORT}`)
+})
