89238e54c25ca9b6063e9a2884bfecd1394a804f
Serhii <mel.gipppson@gmail.com>
2022-11-10 04:07:24 +0200

    added check auth middleware

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ import { validationResult } from 'express-validator'
 import { regValidation } from './validations/auth.js'
 import UserModel from './models/User.js'
 import User from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
 
 dotenv.config()
+
 const passDB = process.env.passDB
-mongoose.connect(`mongodb+srv://Sergo:${passDB}@cluster0.ty0umqn.mongodb.net/blog?retryWrites=true&w=majority`).then(() => console.log('DB connection completed'))
+mongoose.connect(`mongodb+srv://Sergo:${passDB}@cluster0.ty0umqn.mongodb.net/blog?retryWrites=true&w=majority`)
+    .then(() => console.log('DB connection completed'))
     .catch(err => console.log(`DB connection failed: ${err}`))
 
 const PORT = process.env.PORT || 5050
@@ -18,60 +21,74 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId)
+        if(!user){
+            return res.status(404).json({ message: 'User not found' })
+        }
+        const { password, ...userData } = user._doc
+        res.json(userData)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Unsuccessful authorization' })
+    }
+})
+
 app.post('/auth/login', async (req, res) => {
     try {
-        const user = await UserModel.findOne({email: req.body.email})
-        if(!user){
-            return res.status(404).json({message: 'User don\'t found'})
+        const user = await UserModel.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(404).json({ message: 'User don\'t found' })
         }
         const isValidPassword = await bcrypt.compare(req.body.password, user._doc.password)
-        if(!isValidPassword){
-            return res.status(404).json({message: 'Invalid email or password'})
+        if (!isValidPassword) {
+            return res.status(404).json({ message: 'Invalid email or password' })
         }
         const token = jwt.sign(
             { _id: user._id },
             'secret123',
-            {expiresIn: '30d'}
+            { expiresIn: '30d' }
         )
-        const {password, ...userData} = user._doc
-        res.json({...userData, token})
+        const { password, ...userData } = user._doc
+        res.json({ ...userData, token })
 
     } catch (error) {
         console.log(error)
-        res.status(500).json({message: 'Unsuccessful authorization'})
+        res.status(500).json({ message: 'Unsuccessful authorization' })
     }
 })
 
 app.post('/auth/register', regValidation, async (req, res) => {
     try {
         const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
-    }
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
 
-    const pass = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(pass, salt)
-    const doc = new UserModel({
-        firstName: req.body.firstName,
-        email: req.body.email,
-        avatarUrl: req.body.avatarUrl,
-        password: hash,
-    })
-    const user = await doc.save()
-    const token = jwt.sign(
-        { _id: user._id },
-        'secret123',
-        {expiresIn: '30d'}
-    )
+        const pass = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(pass, salt)
+        const doc = new UserModel({
+            firstName: req.body.firstName,
+            email: req.body.email,
+            avatarUrl: req.body.avatarUrl,
+            password: hash,
+        })
+        const user = await doc.save()
+        const token = jwt.sign(
+            { _id: user._id },
+            'secret123',
+            { expiresIn: '30d' }
+        )
 
-    const {password, ...userData} = user._doc
-    res.json({...userData, token})
+        const { password, ...userData } = user._doc
+        res.json({ ...userData, token })
     } catch (error) {
         console.log(error)
-        res.status(500).json({message: 'Unsuccessful registration'})
+        res.status(500).json({ message: 'Unsuccessful registration' })
     }
-    
+
 })
 
 app.listen(PORT, (err) => {
