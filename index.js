5645cd020db97f155fbd6f353ee4fea11556de10
Serhii <mel.gipppson@gmail.com>
2022-11-10 02:39:39 +0200

    installed bcrypt, added authorization and registration logic for user

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 import { validationResult } from 'express-validator'
 import { regValidation } from './validations/auth.js'
+import UserModel from './models/User.js'
+import User from './models/User.js'
 
 dotenv.config()
 const passDB = process.env.passDB
-mongoose.connect(`mongodb+srv://Sergo:${passDB}@cluster0.ty0umqn.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('DB connection completed'))
+mongoose.connect(`mongodb+srv://Sergo:${passDB}@cluster0.ty0umqn.mongodb.net/blog?retryWrites=true&w=majority`).then(() => console.log('DB connection completed'))
     .catch(err => console.log(`DB connection failed: ${err}`))
 
 const PORT = process.env.PORT || 5050
@@ -15,12 +18,60 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/register', regValidation, (req, res) => {
-    const errors = validationResult(req)
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({email: req.body.email})
+        if(!user){
+            return res.status(404).json({message: 'User don\'t found'})
+        }
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.password)
+        if(!isValidPassword){
+            return res.status(404).json({message: 'Invalid email or password'})
+        }
+        const token = jwt.sign(
+            { _id: user._id },
+            'secret123',
+            {expiresIn: '30d'}
+        )
+        const {password, ...userData} = user._doc
+        res.json({...userData, token})
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Unsuccessful authorization'})
+    }
+})
+
+app.post('/auth/register', regValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array())
     }
-    res.json({ success: true })
+
+    const pass = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(pass, salt)
+    const doc = new UserModel({
+        firstName: req.body.firstName,
+        email: req.body.email,
+        avatarUrl: req.body.avatarUrl,
+        password: hash,
+    })
+    const user = await doc.save()
+    const token = jwt.sign(
+        { _id: user._id },
+        'secret123',
+        {expiresIn: '30d'}
+    )
+
+    const {password, ...userData} = user._doc
+    res.json({...userData, token})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Unsuccessful registration'})
+    }
+    
 })
 
 app.listen(PORT, (err) => {
